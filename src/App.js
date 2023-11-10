0e835a74e802a1cf0bf308549db3e748f0d3de10
LorenzovandenDungen <2130766@talnet.nl>
2023-11-10 18:39:51 +0100

    save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [books, setBooks] = useState([]);
+  const [newBook, setNewBook] = useState("");
+
+  const handleAddBook = () => {
+    if (!newBook) return;
+    setBooks([...books, { title: newBook, read: false }]);
+    setNewBook("");
+  };
+
+  const handleDeleteBook = (index) => {
+    const newBooks = books.filter((_, i) => i !== index);
+    setBooks(newBooks);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Personal Library Tracker</h1>
+      <div>
+        <input
+          type="text"
+          value={newBook}
+          onChange={(e) => setNewBook(e.target.value)}
+          placeholder="Add a new book..."
+        />
+        <button onClick={handleAddBook}>Add Book</button>
+      </div>
+      <ul>
+        {books.map((book, index) => (
+          <li key={index}>
+            {book.title}
+            <button onClick={() => handleDeleteBook(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
